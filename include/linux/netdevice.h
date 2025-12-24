5dfb2fca9c481b01ed4b98e2b4d2ec65a81e271c
DawnT112 <3193782263@qq.com>
2025-12-24 17:12:03 +0800

    BPF:CONFIG_XDP_SOCKETS  fix test

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -695,6 +695,9 @@ struct netdev_rx_queue {
 	struct kobject			kobj;
 	struct net_device		*dev;
 	struct xdp_rxq_info		xdp_rxq;
+	#ifdef CONFIG_XDP_SOCKETS
+	struct xdp_umem         *umem;
+	#endif
 } ____cacheline_aligned_in_smp;
 
 /*
